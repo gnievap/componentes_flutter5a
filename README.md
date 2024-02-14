c4bfb50a332a8653901d283116afc956bcbd264a
Gaby Nieva <gaby.nieva@outlook.com>
2024-02-14 12:10:59 -0600

    Starting the app

---
+++ README.md
@@ -0,0 +1,5 @@
+# practica3_5a24
+
+Práctica 3: se verá el uso de varios componentes de flutter,
+como ListView, ListTile, Navigator y Card.
+
---
+++ android/app/src/main/kotlin/com/example/practica3_5a24/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practica3_5a24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
