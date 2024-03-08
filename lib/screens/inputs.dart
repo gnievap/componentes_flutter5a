955c485a1e752db844c6107cf526e1e80708f9f0
Gaby Nieva <gaby.nieva@outlook.com>
2024-03-08 13:57:50 -0600

    Functional BottomNavigationBar

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:practica3_5a24/screens/home_screen.dart';
+import 'package:practica3_5a24/screens/images_screen.dart';
+import 'package:practica3_5a24/screens/infinite_list.dart';
+import 'package:practica3_5a24/screens/notifications.dart';
 import 'package:practica3_5a24/theme/app_theme.dart';
 
 class Inputs extends StatefulWidget {
@@ -15,6 +19,7 @@ class _InputsState extends State<Inputs> {
   bool postreCheck1 = false;
   bool postreCheck2 = false;
   bool postreCheck3 = false;
+  int selectedIndex = 0; // Elemento seleccionado de la BottomNavigationBar
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,18 +56,61 @@ class _InputsState extends State<Inputs> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) => openScreen(context, index),
+        backgroundColor: AppTheme.primaryColor,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            backgroundColor: AppTheme.primaryColor,
+            icon: Icon(
+              Icons.home,
+              color: AppTheme.backColor,
+            ),
             label: 'Inicio',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.next_plan),
-            label: 'Datos',
+            icon: Icon(Icons.list),
+            label: 'Lista',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notification_add),
+            label: 'Notificaciones',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: 'Imágenes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.exit_to_app),
+            label: 'Salir',
           ),
         ],
       ),
     );
+  }
+
+  openScreen(BuildContext context, int index) {
+    // valor por default de ruta
+    MaterialPageRoute ruta =
+        MaterialPageRoute(builder: (context) => const HomeScreen());
+    switch (index) {
+      case 0:
+        ruta = MaterialPageRoute(builder: (context) => const HomeScreen());
+        break;
+      case 1:
+        ruta = MaterialPageRoute(builder: (context) => const InfiniteList());
+        break;
+      case 2:
+        ruta = MaterialPageRoute(builder: (context) => const Notifications());
+        break;
+      case 3:
+        ruta = MaterialPageRoute(builder: (context) => const ImagesScreen());
+        break;
+    }
+    setState(() {
+      selectedIndex = index;
+      Navigator.push(context, ruta);
+    });
   }
 
   TextField entradaTexto() {
